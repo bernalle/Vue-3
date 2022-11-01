5225b8dcdf839091bd34664dbf210099e8bc10a3
gabrielbernalle <gabrielbbmatozinhos@gmail.com>
2022-11-01 13:39:38 -0300

    Aprendendo sobre scoped

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Bem vindo ao primeiro projeto!</h1>
+    <Usuario/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Usuario from './components/Usuario.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Usuario,
+},
+  data() {
+    return {
+        nome:'Sujeito programador'
+      }
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue3/src/components/Usuario.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="usuario">
+    <h2> Olá: Sujeito programador</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Usuario',
+};
+
+</script>
+
+<style scoped>
+  h2{
+    color:red;
+  }
+</style>
